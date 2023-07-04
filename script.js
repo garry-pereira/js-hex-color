01fda762f2083442e23fe270ed7c18b8dee772fd
malmari <garrymp12@gmail.com>
2023-07-03 21:56:40 -0500

    connected all buttons

--- script.js
+++ script.js
@@ -1,61 +1,56 @@
 // get my DOM elements
-const buttons = document.querySelectorAll("button");
-const body = document.querySelector("body");
-const text = document.querySelector(".test123");
-const allDigits = document.querySelectorAll("h1");
-console.log(allDigits);
-const digitArray = allDigits.forEach((digit) => digit.push);
-console.log(digitArray);
+const buttons = document.querySelectorAll('button')
+const body = document.querySelector('body')
+const text = document.querySelector('.test123')
+const allDigits = document.querySelectorAll('h1')
+console.log(allDigits)
+console.log(buttons)
+const buttonsArray = Array.from(buttons)
+console.log(buttonsArray)
 
 // Create Color object
 const color = {
   decimal: 0,
-  pureHex: "0",
-  hex: "#000000",
-};
+  pureHex: '0',
+  hex: '#000000',
+}
 
 // Turn decimal number into pure hex string
 const decToPureHex = (color) => {
-  color.pureHex = color.decimal.toString(16);
-  return color;
-};
+  color.pureHex = color.decimal.toString(16)
+  return color
+}
 
 // Turn pureHex into hex
 const pureHexToHex = (color) => {
-  color.hex = `#${color.pureHex}`;
-};
+  color.hex = `#${color.pureHex}`
+}
 
 // Digit stepper
-const digitStep = () => {};
+// const digitStep = () => {}
 
 // Generate new random color
 const genRandom = (color) => {
-  color.decimal = Math.floor(Math.random() * 16777215);
-  decToPureHex(color);
-  pureHexToHex(color);
-  return color;
-};
+  color.decimal = Math.floor(Math.random() * 16777215)
+  decToPureHex(color)
+  pureHexToHex(color)
+  return color
+}
 
 // Function for updating the DOM
 const updateDom = (color) => {
-  body.style.backgroundColor = color.hex;
-  text.innerText = color.pureHex;
-};
+  body.style.backgroundColor = color.hex
+  text.innerText = color.pureHex
+}
 
 // Function called on buttons being clicked, changes of color
 const colorChange = (e) => {
-  e.preventDefault();
-  console.log(e);
-  switch (e.target.value) {
-    case "gen":
-      genRandom(color);
-      break;
-    case "oneplus" || "onemin":
-      digitStep();
-  }
-  updateDom(color);
-};
+  e.preventDefault()
+  console.log(e)
+  genRandom(color)
+  updateDom(color)
+}
 
-buttons.forEach((button) => {
-  button.addEventListener("click", colorChange);
-});
+buttonsArray.forEach((button) => {
+  button.addEventListener('click', colorChange)
+})
